ff6370088c607bcd447f4dd14a0950183932d276
AliiHussein <aliunali@yahoo.com>
2023-09-10 22:50:34 +0300

    adding comments

--- solid half daimond.cpp
+++ solid half daimond.cpp
@@ -4,7 +4,7 @@ using namespace std;
 void solid_hollow_daimond(int len);
 
 int main(){
-
+    // call the shape function and choose the desired scale of the shape
     solid_hollow_daimond(5);
 
     return 0;
@@ -14,6 +14,7 @@ int main(){
 void solid_hollow_daimond(int len){
     for(int i = 0; i < len*2-1; i++){
         for(int j = 0; j < len; j++){
+            // Upper half of the diamond
             if(i < len){
                 if(j <= i){
                     cout << "*";
@@ -22,6 +23,7 @@ void solid_hollow_daimond(int len){
                     cout << " ";
                 }
             }
+            // Lower half of the diamond
             else{
                 if(j < (len*2 - i -1)){
                     cout << "*";
@@ -32,6 +34,7 @@ void solid_hollow_daimond(int len){
             }
             
         }
+        // New line
         cout << endl;
     }
 }
